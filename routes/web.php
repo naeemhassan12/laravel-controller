e95d154b91d675fb0d750752db1dbc34e1680350
naeemhassan12 <your-email@gmail.com>
2026-01-20 13:23:18 +0500

    adding controller page and working in model

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/about', [UserController::class, 'showCase']);
+Route::get('/contact', [UserController::class, 'userCase']);
